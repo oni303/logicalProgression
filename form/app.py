6584251090054c80a41e10a78bc74f5574a917b3
Georg von Zengen <oni303@gmail.com>
2018-11-03 16:15:17 +0100

    app: using the config.yaml

--- form/app.py
+++ form/app.py
@@ -11,17 +11,20 @@ import sys
 import yaml
  
 
-baseUrl = ''
-dbServer = ''
-dbUser = ''
-dbPw = ""
-dbName = ""
 
 # App config.
-DEBUG = True
+#DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = 'changeME'
+
+with open("config.yaml", 'r') as stream:
+    config = yaml.load(stream)
+    app.config['baseUrl'] = config['baseUrl'] 
+    app.config['dbServer'] = config['dbServer'] 
+    app.config['dbUser'] = config['dbUser'] 
+    app.config['dbPw'] = config['dbPw']
+    app.config['dbName'] = config['dbName']
  
 class ReusableForm(Form):
     date = DateField('date-1', validators=[validators.required()])
@@ -70,7 +73,7 @@ def hello():
         
         idate = int(time.mktime(datetime.datetime.strptime(date, "%Y-%m-%d").timetuple()))
         session = {'date':idate, 'type':sessionType, 'duration':duration, 'routes':routes}
-        r = requests.put(baseUrl + '/session', data = json.dumps(session))
+        r = requests.put(app.config['baseUrl'] + '/session', data = json.dumps(session))
         flash('Saved ' + date)
 
     return render_template('index.html', form=form)
@@ -78,7 +81,7 @@ def hello():
 @app.route('/session', methods=['PUT'])
 def addSession():
 
-    db = pymysql.connect(dbServer,dbUser,dbPw,dbName)
+    db = pymysql.connect(app.config['dbServer'],app.config['dbUser'],app.config['dbPw'],app.config['dbName'])
     dbc = db.cursor() 
     session = json.loads(request.data.decode())
 
@@ -99,21 +102,4 @@ def addSession():
     return json.dumps({'state':"OK"})
  
 if __name__ == "__main__":
-    global baseUrl 
-    global dbServer 
-    global dbUser 
-    global dbPw 
-    global dbName 
-    with open("example.yaml", 'r') as stream:
-        try:
-            config = yaml.load(stream)
-            baseUrl = config['baseUrl'] 
-            dbServer = config['dbServer'] 
-            dbUser = config['dbuser'] 
-            dbPw = config['dbPw']
-            dbName = config['dbName']
-
-            app.run()
-        except yaml.YAMLError as exc:
-            print("ERROR: not able to load config")
-            sys.exit(-1)
+    app.run()
